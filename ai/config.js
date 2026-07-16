355290e01062f6cefd2f5c987a724c0080abfb88
beishan12 <3126669607@qq.com>
2026-07-16 22:53:40 +0800

    Add files via upload

---
+++ ai/config.js
@@ -0,0 +1,6 @@
+// config.js
+const CONFIG = {
+    API_KEY: "sk-jXWn3RecVj8nSUXidW68kZDVjiF3hcWJhMNGu9PJ3RJ22DJ9",
+    API_URL: "https://api.haohaojisui.cn/v1/chat/completions",
+    MODEL: "deepseek-v4-pro"
+}
